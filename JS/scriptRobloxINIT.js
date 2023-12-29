a038f21629bcdd4e2dcb4858dcf52968a3c96256
ranxlus <141138762+ranxlus@users.noreply.github.com>
2023-12-29 21:12:16 +0000

    Create scriptRobloxINIT.js

---
+++ JS/scriptRobloxINIT.js
@@ -0,0 +1,28 @@
+ function initRoblox() {
+      const placeId = document.getElementById("placeid").value;
+      if (placeId) {
+        window.location.href = "roblox://placeId=" + placeId;
+      } else {
+        alert("Please enter a Place ID.");
+      }
+    }
+
+ function toggleSidebar() {
+      const sidebar = document.getElementById('sidebar');
+      const content = document.getElementById('main-content');
+
+      if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+        content.style.marginLeft = '0';
+      } else {
+        sidebar.style.width = '250px';
+        content.style.marginLeft = '250px';
+      }
+    }
+    function closeSidebar() {
+      const sidebar = document.getElementById('sidebar');
+      const content = document.getElementById('main-content');
+
+      sidebar.style.width = '0';
+      content.style.marginLeft = '0';
+    }
