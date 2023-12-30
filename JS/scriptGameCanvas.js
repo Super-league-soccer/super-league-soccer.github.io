4928269090544d3458f75408ca0b6394ac0f8f52
ranxlus <141138762+ranxlus@users.noreply.github.com>
2023-12-30 04:00:21 +0000

    Create scriptGameCanvas.js

---
+++ JS/scriptGameCanvas.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+const ballRadius = 10;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  // Move the ball
+  x += dx;
+  y += dy;
+
+  // Bounce the ball off the walls
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  requestAnimationFrame(draw);
+}
+
+draw();
