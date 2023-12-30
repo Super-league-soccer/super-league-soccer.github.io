db8ef38c3f9966b8c00ccccdc333330b5e47577d
ranxlus <141138762+ranxlus@users.noreply.github.com>
2023-12-30 04:07:33 +0000

    Update scriptGameCanvas.js

--- JS/scriptGameCanvas.js
+++ JS/scriptGameCanvas.js
@@ -35,3 +35,24 @@ function draw() {
 }
 
 draw();
+
+ function toggleSidebar() {
+      const sidebar = document.getElementById('sidebar');
+      const content = document.getElementById('main-content');
+
+      if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+        content.style.marginLeft = '0';
+      } else {
+        sidebar.style.width = '250px';
+        content.style.marginLeft = '250px';
+      }
+    }
+
+    function closeSidebar() {
+      const sidebar = document.getElementById('sidebar');
+      const content = document.getElementById('main-content');
+
+      sidebar.style.width = '0';
+      content.style.marginLeft = '0';
+    }
