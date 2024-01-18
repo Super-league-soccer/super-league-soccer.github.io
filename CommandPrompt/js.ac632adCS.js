f042448788ce86a2d9a10793664637d2cad9fb64
ranxlus <141138762+ranxlus@users.noreply.github.com>
2024-01-18 20:31:38 +0000

    Update js.ac632adCS.js

--- CommandPrompt/js.ac632adCS.js
+++ CommandPrompt/js.ac632adCS.js
@@ -1,18 +1,8 @@
     function addStylesheets(stylesheets) {
       stylesheets.forEach(function(href) {
-        var link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = href;
-        document.head.appendChild(link);
-      });
-    }
-
+        var link = document.createElement('link'); link.rel = 'stylesheet'; link.type = 'text/css'; link.href = href; document.head.appendChild(link);
+      });}
     var stylesheetsArray = [
-      'CommandPrompt/BootStrap/cssbody.css',
-      'CommandPrompt/BootStrap/cssinput.css',
-      'CommandPrompt/BootStrap/cssoutput.css',
-      'CommandPrompt/BootStrap/cssmetaprops.css'
+      'CommandPrompt/BootStrap/cssbody.css', 'CommandPrompt/BootStrap/cssinput.css', 'CommandPrompt/BootStrap/cssoutput.css', 'CommandPrompt/BootStrap/cssmetaprops.css'
     ];
-
     addStylesheets(stylesheetsArray);
