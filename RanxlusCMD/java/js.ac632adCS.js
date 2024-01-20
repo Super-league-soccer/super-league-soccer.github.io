b9b176088473085a0d5b163f830a684cfeab6ab6
ranxlus <141138762+ranxlus@users.noreply.github.com>
2024-01-20 14:30:14 +0000

    Update js.ac632adCS.js

--- RanxlusCMD/java/js.ac632adCS.js
+++ RanxlusCMD/java/js.ac632adCS.js
@@ -1,18 +1,1 @@
-function addStylesheets(stylesheets) {
-  stylesheets.forEach(function (href) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = href;
-    document.head.appendChild(link);
-  });
-}
-
-var stylesheetsArray = [
-  'CSS/cssbody.css',
-  'CSS/cssinput.css',
-  'CSS/cssoutput.css',
-  'CSS/cssbotright.css'
-];
-
-addStylesheets(stylesheetsArray);
+function addStylesheets(stylesheets) { stylesheets.forEach(function (href) { var link = document.createElement('link'); link.rel = 'stylesheet'; link.type = 'text/css'; link.href = href; document.head.appendChild(link);});} var stylesheetsArray = [ 'CSS/cssbody.css', 'CSS/cssinput.css', 'CSS/cssoutput.css', 'CSS/cssbotright.css']; addStylesheets(stylesheetsArray);
