ab5c09bb313802b53985e29a9073f2d826508751
ranxlus <maximumuk401@gmail.com>
2024-04-01 14:54:03 +0100

    Update oneko.js

--- JS/oneko.js
+++ JS/oneko.js
@@ -94,7 +94,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = Number.MAX_VALUE;
 
-    let nekoFile = "http://ranxlus.rf.gd/images/oneko.gif"
+    let nekoFile = "http://ranxlus.github.io/imgs/oneko.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
